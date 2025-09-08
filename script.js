0cdbb4663179ffcecab148ff34f107ecf5cb27cf
Ruhul Amin <ruhulamin3052@gmail.com>
2025-09-08 22:25:20 +0600

    added daynamic section

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const loadeCatagorie = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
     .then ((res) => res.json())
     .then ((data) => {
-        console.log(data.categories);
+        // console.log(data.categories);
         const category = data.categories;
         category.forEach (cat => {
             categoriesContainer.innerHTML += `
@@ -14,8 +14,41 @@ const loadeCatagorie = () => {
                 </li>`
         })
     })
+    };
+   
+   const displayAllData = document.getElementById("main-card");
+   displayLesson = () => {
+     fetch("https://openapi.programming-hero.com/api/plants")
+        .then((res) => res.json())
+            .then((plants) => allDisplayLessonUi(plants.plants));
+
+   };
+   const allDisplayLessonUi = (lessons) => {
+    const mainContainer = document.getElementById("main-card");
+    mainContainer.innerHTML = ""; 
+
+    for (let lesson of lessons) {
+        mainContainer.innerHTML += `
+            <div class="bg-white card p-4 shadow-xl">
+                <img src="${lesson.image}" alt="${lesson.name}" class="w-full h-48 object-cover rounded-lg my-2" />
+                <h2 class="font-semi-bold text-2xl cursor-pointer">${lesson.name}</h2>
+                <p>${lesson.description}</p>
+                <div class="flex justify-between mt-2 items-center">
+                 <button class="py-1 px-4 bg-[#DCFCE7] rounded-full text-[#15803D]">${lesson.category}</button>
+                <h2 class="font-bold">
+                    <span class="text-2xl font-bold">৳</span>
+                    <span class="text-xl text-black">${lesson.price}</span>
+                </h2>
+                </div>
+                <button class="bg-[#15803D] rounded-full py-2 w-full text-white cursor-pointer mt-2">Add to Cart</button>
+            </div>
+        `;
+    }
 };
-loadeCatagorie();
+   
+   displayLesson();
+    loadeCatagorie();
+
 
 
 
